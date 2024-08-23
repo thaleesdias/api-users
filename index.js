f41508b4dd240d75c502a48f226008787b3dc130
Thales Dias <thalesmdias@gmail.com>
2024-08-22 22:13:43 -0300

    rotas movidas pro arquivo src/routes/routes.js

--- index.js
+++ index.js
@@ -1,78 +1,14 @@
+import express from 'express'
+import router from './src/routes/routes.js'
 
-import express, { query } from 'express'
-import db from './src/db/connection.js'
 
 const app = express()
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/', router)
 
-app.get('/users', (req, res) => {
-    const query = "SELECT * FROM users;"
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err)
-            res.status(500).send('erro ao buscar dados')
-            return
-        }
-        res.json(results)
-
-    })
-})
-
-app.get('/user', (req, res) => {
-
-    const id = req.body.id
-    console.log(id)
-    if (!id) {
-        return res.status(400).json({ error: "ID is required" });
-    }
-
-    const query = `SELECT * FROM users WHERE id=${id}`
-
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err)
-            return res.status(404)
-        }
-        return res.status(200).json(results)
-    })
-})
-
-
-app.post('/user/save', (req, res) => {
-    const name = req.body.name || 'teste'
-    const lastName = req.body.lastName || 'messi'
-    const cellNumber = req.body.number || '00000-11111'
-    console.log('e')
-    const query = `INSERT INTO users (name, lastName, number) VALUES ('${name}', '${lastName}', '${cellNumber}')`
-
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).json({ error: 'vish' })
-        }
-        return res.status(201).send("usuario cadastrado!")
-    })
-})
-
-app.delete('/user/delete', (req, res) => {
-    const id = req.body.id
-    if (!id) {
-        console.log('not found')
-        return res.status(404).json({ err: "sem id" })
-    }
-
-    const query = `DELETE FROM users WHERE id="${id}"`
-
-    db.query(query,  (err, results)=>{
-        if(err){
-            console.log("F")
-            return res.status(400).json({"erro":"erro na query"})
-        }
-        return res.status(200).json({status:"okay"})
-    })
-})
 
 
 
